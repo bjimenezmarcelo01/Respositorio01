bfa70483c9a58c6f8eac4283fe1b7d6c58d7c725
lucasalcala25 <lucasalcala25@gmail.com>
2021-03-09 20:05:36 -0400

    Prueba Lucas

---
+++ WindowsFormsApp1/PruebaLucas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class PruebaLucas
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prueba");
+        }
+    }
+}
